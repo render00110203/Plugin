6befc340441ffe13d873ed0bc132a5c6f17466ea
render00110203 <render00112025@hotmail.com>
2025-11-01 15:41:04 +0530

    Create build.gradle.kts

---
+++ GDriveEnhanced/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("com.github.recloudstream:cloudstream:latest.release")
+}
